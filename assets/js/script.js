cd35199d4d091e774e3477c6edfcc64fa08b27d8
ktrnthsnr <asakthisner@hotmail.com>
2020-04-25 20:16:16 -0700

    added materialize sid nav and select options

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,14 @@
   $(document).ready(function(){
     $('.parallax').parallax();
     $('.sidenav').sidenav();
+    $('select').formSelect();
   });
 
 
 //parallax method
 // var instance = M.Parallax.getInstance(elem);
 
+//select button materialize
+// $(document).ready(function(){
+//   $('select').formSelect();
+// });
